cf1e2c52452e716204057d77b4ecb2db2253583c
Maab Eltayeb <maabosman.edu@gmail.com>
2025-11-29 19:42:16 +0200

    chore: separate vitest config to fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   // Set base for GitHub Pages deployment. Replace with repo name if different.
   base: '/Personal-Trainer/',
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    globals: true,
-    coverage: {
-      reporter: ['text','html'],
-    },
-  },
-})
+});
